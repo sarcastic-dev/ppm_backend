785e883a85cf39f40fa8eba67a4b574a693c4113
Shubham jain <shubjain440@gmail.com>
2020-07-14 11:42:01 +0530

    Minor fix

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,7 +3,8 @@ class HomesController < ApplicationController
   before_action :authorize_request
 
   def index
-    render json: {name: current_user.name}, status: 200
+    render json: { status: true, name: current_user.name}, status: 200
   end
+
 
 end
